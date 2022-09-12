5c1b7769b9bbc6c282fa93b5d6eda11ca070a72b
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2022-09-12 20:40:53 +0000

    Add: Register end point added

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+
+const dbConnect = require("./db/dbConnect")
+const User = require("./db/userModel");
+
+dbConnect();
 
 // body parser configuration
 app.use(bodyParser.json());
@@ -11,5 +17,34 @@ app.get("/", (request, response, next) => {
   next();
 });
 
+app.post("/register", (request, response) => {
+  bcrypt.hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      const user = new User({
+        name: request.body.name,
+        email: request.body.email,
+        password: hashedPassword
+      });
+      user.save()
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Succesfully",
+            result,
+          });
+        })
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed succesfully",
+        e,
+      });
+    });
+});
 
 module.exports = app;
